de178584c984779fcba47243ca828713356d5d27
sohel5G <contactsohelrana@gmail.com>
2023-10-18 23:38:40 +0600

    Server setup and running on browser

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Aircraft Engineers Store Server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Aircraft Engineers Store Server is running on PORT: ${port}`)
+})
+
