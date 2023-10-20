016dea2537f1f5b20203b64b4877f58535790b42
sohel5G <contactsohelrana@gmail.com>
2023-10-20 09:06:53 +0600

    version configuration file added

--- index.js
+++ index.js
@@ -31,17 +31,22 @@ const client = new MongoClient(uri, {
 
 const productsCollection = client.db("aircraftengineersstoreDB").collection("products");
 const brandsCollection = client.db("aircraftengineersstoreDB").collection("brands");
+const cartItemCollection = client.db("aircraftengineersstoreDB").collection("carditems");
 
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        // Add products API
         app.post('/products', async (req, res) => {
             const product = req.body;
             const result = await productsCollection.insertOne(product);
             res.send(result);
         })
+
+
+        // Show All products API
         app.get('/products', async (req, res) => {
             const products = productsCollection.find()
             const result = await products.toArray()
@@ -49,11 +54,46 @@ async function run() {
         })
 
 
+        // View single product details or show product details in the update page input API
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+        })
+
+
+        // Update product API
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const product = req.body;
+
+            const options = { upsert: true };
+            const updateProduct = {
+                $set: {
+                    name: product.name,
+                    brand: product.brand,
+                    type: product.type,
+                    price: product.price,
+                    ratting: product.ratting,
+                    productImgURL: product.productImgURL,
+                    description: product.description
+                }
+            };
+            const result = await productsCollection.updateOne(query, updateProduct, options);
+            res.send(result);
+        })
+
+
+        // Brands API Show all brand
         app.get('/brands', async (req, res) => {
             const brands = brandsCollection.find()
             const result = await brands.toArray()
             res.send(result)
         })
+
+        // Brand single page Show only this brand products 
         app.get('/brands/:brandName', async (req, res) => {
             const brandName = req.params.brandName;
             const query = { brand: brandName };
@@ -62,6 +102,28 @@ async function run() {
             res.send(result);
         })
 
+
+        // Add To cart products store in database API
+        app.post('/carditems', async (req, res) => {
+            const product = req.body;
+            const result = await cartItemCollection.insertOne(product);
+            res.send(result);
+        })
+
+        // Cart page API Show all Cart item
+        app.get('/carditems', async (req, res) => {
+            const carts = cartItemCollection.find()
+            const result = await carts.toArray()
+            res.send(result)
+        })
+
+        // Find Cart item to delete from cart page
+        app.delete('/carditems/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await cartItemCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
